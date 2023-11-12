55803aa2040c44a13651db073154c3cb978404b6
lharmeedey <kolawolebasit@gmail.com>
2023-11-12 06:18:58 +0100

    comments added

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { view } from '@forge/bridge';
-import View from './View';
-import Edit from './Edit';
+import View from './View'; // Import the View component
+import Edit from './Edit'; // Import the Edit component
 
+// Main application component
 function App() {
+  // State variable to store the context received from the Forge platform
   const [context, setContext] = useState();
 
+  // Fetch the context using the view.getContext() function when the component mounts
   useEffect(() => {
     view.getContext().then(setContext);
   }, []);
 
+  // If the context is not available yet, show a loading message
   if (!context) {
     return 'Loading...';
   }
 
-  return context.extension.entryPoint === 'edit' ? <Edit/> : <View/>;
+  // Determine the entry point based on the context and render either the Edit or View component
+  return context.extension.entryPoint === 'edit' ? <Edit /> : <View />;
 }
 
 export default App;
--- static/hello-world/src/index.js
+++ static/hello-world/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import Atlaskit's CSS reset to ensure consistent styling across browsers
 import '@atlaskit/css-reset';
 
+// Render the main App component inside the root element of the HTML document
 ReactDOM.render(
+  // Enable React StrictMode for additional development checks
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
+  // Mount the rendered component into the HTML element with the id 'root'
   document.getElementById('root')
 );
